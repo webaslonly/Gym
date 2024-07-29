09e2a282dc1f9576661eaf8a064c29361c0f4b40
webaslonly <aslpubg011@gmail.com>
2024-07-29 18:57:26 +0500

    regisrter and login

--- src/components/shared/mode-toggle.tsx
+++ src/components/shared/mode-toggle.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/hooks/use-theme"
+import { useTheme } from "@/hook/use-theme"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
